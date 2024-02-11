e602d9389363f8d711f3ee5235ee8e8280e036ca
hjferguson <harlanjazz@gmail.com>
2024-02-11 18:52:17 -0500

    navbar redesign with drop down menu

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
 import LoginButton from './login_out/LoginButton';
 import LogoutButton from './login_out/LogoutButton';
@@ -7,7 +7,11 @@ import './Navbar.css';
 import logo from '../mygame.png';
 
 const Navbar = () => {
-  const { isAuthenticated } = useAuth0();
+  const { isAuthenticated, user } = useAuth0();
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  // Toggle dropdown menu
+  const toggleDropdown = () => setShowDropdown(!showDropdown);
 
   return (
     <nav>
@@ -15,16 +19,30 @@ const Navbar = () => {
         <img src={logo} alt="Logo" style={{ height: '50px' }} />
       </Link>
 
-      {/* Dummy search bar */}
       <input type="text" placeholder="Search..." disabled />
 
-      {/* Conditionally render auth buttons */}
       {!isAuthenticated && <LoginButton />}
+      
       {isAuthenticated && (
-        <>
-          <Link to="/profile">Profile</Link>
-          <LogoutButton />
-        </>
+        <div style={{ position: 'relative' }}>
+          <img 
+            src={user.picture} 
+            alt="Profile" 
+            style={{ height: '50px', borderRadius: '50%', cursor: 'pointer' }} 
+            onClick={toggleDropdown}
+          />
+          {showDropdown && (
+            <div className="dropdown-menu">
+              <p>Hey {user.name}</p>
+              <Link to="/profile" onClick={() => setShowDropdown(false)}>Go to Profile</Link>
+              <br></br>
+              <Link to="/settings" onClick={() => setShowDropdown(false)}>Settings</Link>
+              <br></br>
+              <LogoutButton />
+            </div>
+          )}
+
+        </div>
       )}
     </nav>
   );
