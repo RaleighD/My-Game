3ced390bda5d84b3df0bde67a6f76080d5309545
hjferguson <harlanjazz@gmail.com>
2024-02-11 01:30:04 -0500

    adds nav bar, removes everything from app.js

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # My-Game
 <h2>Your life starts now.</h2>
+
+<h1>Set up</h1>
+<h2>Server<h2>
+Navigate to 'backend' in your terminal.
+Run 'npm install' to install node modules required for project.
+<hr>
+Create a .env file within the backend directory.
+Add this line and update username and password with your mongo atlas project associated user/pass: \n
+MONGODB_URI=mongodb+srv://USERNAME:PASSWORD@mygame.bscgfip.mongodb.net/?retryWrites=true&w=majority
+<hr>
+To start server, run 'npm start'. Runs on port 5001. Why? Who knows. Ask Rocko. 
+<hr>
+<h2>Frontend</h2>
+Navigate to 'frontend' in your terminal.
+Run 'npm install' to install node modules required for project.
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,56 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
-import logo from './mygame.png';
-// Import Auth0 components
-import LoginButton from './components/login_out/LoginButton';
-import LogoutButton from './components/login_out/LogoutButton';
-import ProfileTest from './components/login_out/ProfileTest';
-import ProtectedCreatePost from './components/CreatePost/ProtectedCreatePost';
-
-
-//testing modal
-import Modal from './components/CreatePost/Modal';
-
-
-// Import components for routing
-import CreatePost from './components/CreatePost/CreatePost';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import Layout from './components/Layout'; //layout has auth0 and navbar in it
 
 function App() {
-
-  //testing modal
-  const [isModalOpen, setModalOpen] = useState(false);
-  const handleOpenModal = () => {
-    setModalOpen(true);
-  }
-  const handleCloseModal = () => {
-    setModalOpen(false)
-  }
-
   return (
     <Router>
-      <div className="App">
-        <div>
-        <button onClick={handleOpenModal}>Create Post</button>
-        <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
-          <CreatePost />
-        </Modal>
-        {/* Rest of your application */}
-      </div>
-        <header className="App-header">
-          <img src={logo} alt="MyGame Logo" className="App-logo" />
-          <LoginButton />
-          <LogoutButton />
-          <ProfileTest />
-          
-        </header>
+      <Layout> 
         <Routes>
-          
-          <Route path="/create-post" element={<CreatePost />} />
-          {/* other routes */}
+          <Route path="/" element={<HomePage />} />
+          {/* Define other routes here */}
         </Routes>
-      </div>
+      </Layout>
     </Router>
   );
 }
