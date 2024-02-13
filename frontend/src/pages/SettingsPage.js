1d4ea640b3375e504638b75825f0ef3f22bee8a5
hjferguson <harlanjazz@gmail.com>
2024-02-12 20:19:53 -0500

    refines delete account

--- frontend/src/pages/SettingsPage.js
+++ frontend/src/pages/SettingsPage.js
@@ -1,22 +1,28 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import { ThemeContext } from '../components/layout/ThemeContext';
+import Modal from '../components/CreatePost/Modal';
 
 const SettingsPage = () => {
   const { theme, setTheme } = useContext(ThemeContext);
   const { user, getAccessTokenSilently, logout } = useAuth0(); // Destructure logout from useAuth0
+  const [isDeleteModalOpen, setDeleteModalOpen] = useState(false);
 
   const toggleTheme = () => {
     const newTheme = theme === 'light' ? 'dark' : 'light';
     setTheme(newTheme);
   };
 
-  const deleteAccount = async () => {
-    const isConfirmed = window.confirm("Are you sure you want to delete your account? This action cannot be undone.");
-    if (!isConfirmed) {
-      return; // User canceled the operation
-    }
+  const handleDeleteClick = () => {
+    setDeleteModalOpen(true);
+  }
 
+  const handleConfirmDelete = async () =>{
+    setDeleteModalOpen(false);
+    await deleteAccount();
+  }
+
+  const deleteAccount = async () => {
     try {
       const userId = user.sub;
       const token = await getAccessTokenSilently();
@@ -26,12 +32,12 @@ const SettingsPage = () => {
           Authorization: `Bearer ${token}`,
         },
       });
-
+  
       const data = await response.json();
       if (!response.ok) {
         throw new Error(data.message || "Failed to delete account");
       }
-
+  
       // Account deletion was successful, log the user out
       logout({ returnTo: window.location.origin }); // Redirect user to home page after logout
     } catch (error) {
@@ -39,15 +45,22 @@ const SettingsPage = () => {
       alert("Error deleting account. Please try again.");
     }
   };
+  
 
   return (
     <div>
       <button onClick={toggleTheme}>
         Switch to {theme === 'light' ? 'Dark' : 'Light'} Mode
       </button>
-      <button onClick={deleteAccount} style={{ marginTop: '20px', backgroundColor: 'red', color: 'white' }}>
+      <button onClick={handleDeleteClick} style={{ marginTop: '20px', backgroundColor: 'red', color: 'white' }}>
         Delete Account
       </button>
+      <Modal isOpen={isDeleteModalOpen} onClose={() => setDeleteModalOpen(false)}>
+        <img src="/images/rimtobinson.gif" alt="Funny meme" />
+        <button onClick={() => setDeleteModalOpen(false)}>Cancel</button>
+        <button onClick={handleConfirmDelete}>I'm not worried about it.</button>
+      </Modal>
+
     </div>
   );
 };
