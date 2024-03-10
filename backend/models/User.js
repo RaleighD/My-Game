f19a5d91321a2bbd2bc56f9cef00a541302c7d51
hjferguson <harlanjazz@gmail.com>
2024-03-10 16:39:46 -0400

    updates user model to add roles for future update

--- backend/models/User.js
+++ backend/models/User.js
@@ -28,45 +28,14 @@ const userSchema = new mongoose.Schema({
   isProfileComplete: { // Flag to check if user has completed all required fields
     type: Boolean,
     default: false,
-  }
+  },
+  role: {
+    type: String,
+    enum: ['none', 'player', 'team_organizer', 'league_organizer', 'admin'],
+    default: 'none', //default role, because not all users will be a part of a league or team
+  },
 }, { timestamps: true }); // Adds createdAt and updatedAt timestamps automatically
 
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
-
-
-
-// const mongoose = require('mongoose');
-// const { Schema } = mongoose;
-
-// // Role enum
-// const roles = {
-//   PLAYER: 'player',
-//   TEAM_ORGANIZER: 'team_organizer',
-//   LEAGUE_ORGANIZER: 'league_organizer',
-//   ADMIN: 'admin'
-// };
-
-// const userSchema = new Schema({
-//   // ... existing fields ...
-//   role: {
-//     type: String,
-//     enum: Object.values(roles),
-//     default: roles.PLAYER, // Default role when a user registers
-//     required: true
-//   },
-//   teams: [{ 
-//     type: Schema.Types.ObjectId, 
-//     ref: 'Team' 
-//   }],
-//   leagues: [{ 
-//     type: Schema.Types.ObjectId, 
-//     ref: 'League' 
-//   }]
-//   // ... existing fields ...
-// }, { timestamps: true });
-
-// const User = mongoose.model('User', userSchema);
-
-// module.exports = User;
