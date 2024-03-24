bfedb7eb683080a7c2e95d9d9eadaa38090d1005
hjferguson <harlanjazz@gmail.com>
2024-03-24 16:56:19 -0400

    adds socket.io to backend

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,27 @@
 const app = require('./server');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const server = http.createServer(app); 
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000", 
+        methods: ["GET", "POST"],
+    },
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+  
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+
+   
+});
 
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`)); // Use `server.listen` instead of `app.listen`
 
-
-console.log("Good morning Vietnam!")
+console.log("Good morning Vietnam!");
