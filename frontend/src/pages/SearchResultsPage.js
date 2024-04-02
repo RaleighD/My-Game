bd4fce315dd6b44dd68316fcfd3dee9a499f21f7
RaleighD <raleigh.b.desmond@gmail.com>
2024-04-02 15:26:40 -0400

    Search now works for posts. Creating display post page.

--- frontend/src/pages/SearchResultsPage.js
+++ frontend/src/pages/SearchResultsPage.js
@@ -7,7 +7,7 @@ const useQuery = () => new URLSearchParams(useLocation().search);
 const SearchResultsPage = () => {
     const query = useQuery();
     const searchTerm = query.get("query");
-    const [results, setResults] = useState([]);
+    const [results, setResults] = useState({ users: [], posts: [] });
 
     useEffect(() => {
         const fetchData = async () => {
@@ -28,8 +28,9 @@ const SearchResultsPage = () => {
 
     return (
         <div className="results-container">
-            {results.length > 0 ? (
-                results.map(user => (
+            <h2>Users</h2>
+            {results.users && results.users.length > 0 ? (
+                results.users.map(user => (
                     <div key={user._id} className="result-item">
                         <Link to={`/profile/${user.auth0Id}`} className="user-link">
                             <img src={user.picture} alt={user.nickname} className="user-image" />
@@ -39,6 +40,19 @@ const SearchResultsPage = () => {
                 ))
             ) : (
                 <p>No users found.</p>
+            )}
+            <h2>Posts</h2>
+            {results.posts && results.posts.length > 0 ? (
+                results.posts.map(post => (
+                    <div key={post._id} className="result-item">
+                        <Link to={`/post/${post._id}`} className="post-link">
+                            <div className="post-title">{post.title}</div> {/* Assuming posts have titles */}
+                            <p className="post-description">{post.description}</p>
+                        </Link>
+                    </div>
+                ))
+            ) : (
+                <p>No posts found.</p>
             )}
         </div>
     );
